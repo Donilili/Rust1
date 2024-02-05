56e6112ce928ae01537f4e416e55ca29fc576303
Donilili <124972764+Donilili@users.noreply.github.com>
2024-02-05 19:03:06 +0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,11 @@
+// fn main() {
+//     println! ("Hello World MY GENIUS MR DON!!! YOU ARE THE GREATEST MAN OF ALL TIME YEAH MAN! ");
+// }
+
+fn main() {
+    let x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    }
+    
